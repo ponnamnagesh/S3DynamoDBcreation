d8a81897933aa32845ec9adfcd68689144491ebe
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-06-09 09:39:50 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,28 +1,23 @@
-variable "aws_region" {
-       description = "The AWS region to create things in." 
-       default     = "us-east-2" 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
 }
 
-variable "key_name" { 
-    description = " SSH keys to connect to ec2 instance" 
-    default     =  "myJune2021Key" 
+variable "alb_name" {
+  description = "The name of the ALB"
+  type        = string
+  default     = "terraform-asg-example"
 }
 
-variable "instance_type" { 
-    description = "instance type for ec2" 
-    default     =  "t2.micro" 
+variable "instance_security_group_name" {
+  description = "The name of the security group for the EC2 Instances"
+  type        = string
+  default     = "terraform-example-instance"
 }
 
-variable "security_group" { 
-    description = "Name of security group" 
-    default     = "my-jenkins-security-group" 
-}
-
-variable "tag_name" { 
-    description = "Tag Name of for Ec2 instance" 
-    default     = "my-ec2-instance" 
-} 
-variable "ami_id" { 
-    description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0b9064170e32bde34" 
+variable "alb_security_group_name" {
+  description = "The name of the security group for the ALB"
+  type        = string
+  default     = "terraform-example-alb"
 }
