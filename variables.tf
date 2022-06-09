f4aa795ec133c5af5d8ce1a1027efb1d24ad715a
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-06-09 09:59:22 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,17 +7,17 @@ variable "server_port" {
 variable "alb_name" {
   description = "The name of the ALB"
   type        = string
-  default     = "terraform-asg-example"
+  default     = "terraform-asg-ClaimVision"
 }
 
 variable "instance_security_group_name" {
   description = "The name of the security group for the EC2 Instances"
   type        = string
-  default     = "terraform-example-instance"
+  default     = "terraform-ClaimVision-instance"
 }
 
 variable "alb_security_group_name" {
   description = "The name of the security group for the ALB"
   type        = string
-  default     = "terraform-example-alb"
+  default     = "terraform-ClaimVision-alb"
 }
