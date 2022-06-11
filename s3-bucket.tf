0adef9c11d68493eec94b771ac969b1a076ad010
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-06-11 13:39:36 -0500

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-   bucket = "Terraform-Jenkins-Ansible-S3"
+   bucket = "terraform-jenkins-ansible-s3"
    acl = "private"
    versioning {
       enabled = true
