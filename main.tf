7a0ddcfb6f4471f7904db4d4d97aa6246cf83cd2
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-06-13 13:57:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,139 +12,28 @@ provider "aws" {
   # version = "~> 2.0"
 }
 
-resource "aws_launch_configuration" "ClaimVision" {
-  image_id        = "ami-0c55b159cbfafe1f0"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-
-  # Required when using a launch configuration with an auto scaling group.
-  # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
-  lifecycle {
-    create_before_destroy = true
-  }
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "great-name-terraform-state-2222"
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  #versioning {
+    #enabled = true
+  #}
+  #server_side_encryption_configuration {
+    #rule {
+      #apply_server_side_encryption_by_default {
+       # sse_algorithm = "AES256"
+      #}
+    #}
+  #}
 }
 
-resource "aws_autoscaling_group" "ClaimVision" {
-  launch_configuration = aws_launch_configuration.ClaimVision.name
-  vpc_zone_identifier  = data.aws_subnets.default.ids
-
-  target_group_arns = [aws_lb_target_group.asg.arn]
-  health_check_type = "ELB"
-
-  min_size = 2
-  max_size = 10
-
-  tag {
-    key                 = "Name"
-    value               = "terraform-asg-ClaimVision"
-    propagate_at_launch = true
-  }
-}
-
-resource "aws_security_group" "instance" {
-  name = var.instance_security_group_name
-
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  #vpc_id = data.aws_vpc.default.id
-  #vpc_id     = aws_vpc.main.id
-}
-
-resource "aws_lb" "example" {
-
-  name               = var.alb_name
-
-  load_balancer_type = "application"
-  subnets            = data.aws_subnets.default.ids
-  security_groups    = [aws_security_group.alb.id]
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  # By default, return a simple 404 page
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "great-name-locks-2"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
     }
-  }
-}
-
-resource "aws_lb_target_group" "asg" {
-
-  name = var.alb_name
-
-  port     = var.server_port
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 15
-    timeout             = 3
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_lb_listener_rule" "asg" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 100
-
-  condition {
-    path_pattern {
-      values = ["*"]
-    }
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.asg.arn
-  }
-}
-
-resource "aws_security_group" "alb" {
-
-  name = var.alb_security_group_name
-
-  # Allow inbound HTTP requests
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound requests
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
