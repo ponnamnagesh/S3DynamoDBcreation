7785e7ebaae2984533b09d9e9e98966242caa0e4
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-06-21 12:41:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,39 +1,45 @@
 terraform {
-  required_version = ">= 1.2.2"
+  required_version = ">= 0.12"
 }
 
 provider "aws" {
-      #access_key = "${var.aws_access_key}"
-    #secret_key = "${var.aws_secret_key}"
-    
   region = "us-east-2"
 
   # Allow any 2.x version of the AWS provider
-  # version = "~> 2.0"
+  #version = "~> 2.0"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "great-name-terraform-state-2222"
+
+  bucket = var.bucket_name
+
+  // This is only here so we can destroy the bucket as part of automated tests. You should not copy this for production
+  // usage
+  force_destroy = true
+
   # Enable versioning so we can see the full revision history of our
   # state files
-  #versioning {
-    #enabled = true
-  #}
-  #server_side_encryption_configuration {
-    #rule {
-      #apply_server_side_encryption_by_default {
-       # sse_algorithm = "AES256"
-      #}
-    #}
-  #}
+  versioning {
+    enabled = true
+  }
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "great-name-locks-2"
+  name         = var.table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
+
   attribute {
     name = "LockID"
     type = "S"
-    }
+  }
 }
