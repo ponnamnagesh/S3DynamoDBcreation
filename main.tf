87d4b9b809b2ac78c654d3af64b7a7e82b3e1023
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-06-21 13:01:02 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,13 +24,13 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 
   # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
+  #server_side_encryption_configuration {
+    #rule {
+      #apply_server_side_encryption_by_default {
+        #sse_algorithm = "AES256"
+      #}
+    #}
+  #}
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
