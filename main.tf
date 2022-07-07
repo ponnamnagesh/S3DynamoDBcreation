fecee2f329b9530bd75cd5545abd20139641fd70
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-07-07 15:08:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "great-name-terraform-state-2222"
-  key     = "service/ansible/terraform.tfstate"
+  #key     = "service/ansible/terraform.tfstate"
   # Enable versioning so we can see the full revision history of our
   # state files
   #versioning {
